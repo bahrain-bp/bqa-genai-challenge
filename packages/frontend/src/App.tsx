1d2b197859ee9d29c288cc1d4589f78cd6bb7054
AyshaCheema <202003059@student.polytechnic.bh>
2024-03-10 14:19:52 +0300

    Adding my name to index file

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -33,6 +33,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+      Aysha Cheema hehe 
+    </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
