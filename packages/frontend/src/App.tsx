5e667a3b66854b43415a0a1f334a527fc7123ffb
MaryamTaraif <maryamtraif02@gmail.com>
2024-03-10 13:59:26 +0300

    added my name to App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState("")
+  const [count, setCount] = useState("");
 
   function onClick() {
     console.log(import.meta.env.VITE_API_URL);
@@ -27,18 +27,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={onClick}>
-          count is {count ? count : "unknown"}
-        </button>
+        <button onClick={onClick}>count is {count ? count : "unknown"}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        My name is Maryam Taraif 
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
