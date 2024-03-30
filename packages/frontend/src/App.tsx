b7711f41fc834a132edb7394991e6d6e7241a45d
MaryamAlsaad <Maryamalsaad@hotmail.co.uk>
2024-03-30 17:17:31 +0300

    Added the layout, created a cognito pool with jwt autherizer

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,49 +1,138 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Settings from './pages/Settings';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
+import PredefinedTemplate from './pages/predefinedTemplate';
 
 function App() {
-  const [count, setCount] = useState("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
 
-  function onClick() {
-    console.log(import.meta.env.VITE_API_URL);
-    fetch(import.meta.env.VITE_API_URL, {
-      method: "POST",
-    })
-      .then((response) => response.text())
-      .then(setCount);
-  }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return (
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={onClick}>count is {count ? count : "unknown"}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          This is Maryam Alsaad.
-        </p>
-        <p>
-      Aysha Cheema hehe 
-    </p>
-        <p>
-          This is Maryam Alsaad.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        My name is Maryam Taraif 
-      </p>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard | EduScribe" />
+              <ECommerce />
+            </>
+          }
+        />
+        
+        <Route
+          path="/PredefinedTemplate"
+          element={
+            <>
+              <PageTitle title="Predefined Template | EduScribe" />
+              <PredefinedTemplate />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-elements"
+          element={
+            <>
+              <PageTitle title="Form Elements | EduScribe" />
+              <FormElements />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-layout"
+          element={
+            <>
+              <PageTitle title="Form Layout | EduScribe" />
+              <FormLayout />
+            </>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables | EduScribe" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | EduScribe" />
+              <Settings />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart | EduScribe" />
+              <Chart />
+            </>
+          }
+        />
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts | EduScribe" />
+              <Alerts />
+            </>
+          }
+        />
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons | EduScribe" />
+              <Buttons />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | EduScribe" />
+              <SignIn />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | EduScribe" />
+              <SignUp />
+            </>
+          }
+        />
+      </Routes>
     </>
   );
 }
---
+++ stacks/AuthStack.ts
@@ -0,0 +1,55 @@
+import { Api, Cognito, StackContext } from "sst/constructs";
+import { StaticSite } from "sst/constructs";
+
+export function AuthStack({ stack }: StackContext) {
+  // Create Api
+   // Create auth provider
+const auth = new Cognito(stack, "Auth", {
+    login: ["email"],
+  });
+  const api = new Api(stack, "Api_auth", {
+    authorizers: {
+        jwt: {
+          type: "user_pool",
+          userPool: {
+            id: auth.userPoolId,
+            clientIds: [auth.userPoolClientId],
+          },
+        },
+      },
+    defaults: {
+      authorizer: "jwt",
+    },
+    routes: {
+      "GET /private": "packages/functions/src/private.main",
+      "GET /public": {
+        function: "packages/functions/src/public.main",
+        authorizer: "none",
+      },
+    },
+  });
+
+  const site = new StaticSite(stack, "Site", {
+    path: "frontend",
+    environment: {
+      VITE_APP_API_URL: api.url,
+      VITE_APP_USER_POOL_ID: auth.userPoolId,
+      VITE_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+    },
+  });
+  
+  // Show the API endpoint and other info in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    UserPoolId: auth.userPoolId,
+    UserPoolClientId: auth.userPoolClientId,
+    SiteUrl: site.url,
+  });
+
+  
+  // Allow authenticated users invoke API
+  auth.attachPermissionsForAuthUsers(stack, [api]);
+
+  // Show the API endpoint and other info in the output
+
+}
