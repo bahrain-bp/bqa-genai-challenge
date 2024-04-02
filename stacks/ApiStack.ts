56a91ed5caad08cd2588bb28beac09f62309182c
AyshaCheema <202003059@student.polytechnic.bh>
2024-04-02 19:51:34 +0300

    s3 bucket creation in sst

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -6,7 +6,7 @@ import { Duration } from "aws-cdk-lib/core";
 export function ApiStack({ stack }: StackContext) {
 
     const {table} = use(DBStack);
-    
+   
     // Create the HTTP API
     const api = new Api(stack, "Api", {
         defaults: {
