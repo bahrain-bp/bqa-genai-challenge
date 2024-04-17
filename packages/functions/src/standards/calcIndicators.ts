18f5216d6a2573bc336c72d6e816250fe88980be
AmjadShubbar <120450558+AmjadShubbar@users.noreply.github.com>
2024-04-17 12:42:10 +0300

    Update calcIndicators.ts

--- packages/functions/src/standards/calcIndicators.ts
+++ packages/functions/src/standards/calcIndicators.ts
@@ -5,10 +5,8 @@ export const main: APIGatewayProxyHandlerV2 = async (event) => {
     // Define the API endpoint URL (replace with your actual API endpoint)
     const apiUrl = "https://iklcb83307.execute-api.us-east-1.amazonaws.com/standards";
 
-    // Use fetch to send a GET request to the API
     const response = await fetch(apiUrl);
 
-    // Check for successful response (status code 200)
     if (!response.ok) {
       console.error(`Error fetching standards from API: ${response.status}`);
       return {
@@ -20,7 +18,6 @@ export const main: APIGatewayProxyHandlerV2 = async (event) => {
     // Parse the response body (assuming JSON format)
     const standards = await response.json() as { indicators: string[] }[];
 
-    // Handle empty response (optional)
   
 
     // Calculate total number of indicators across all standards
