3dd948e18bc6ac5c51ead56997d6675888951d24
AyshaCheema <202003059@student.polytechnic.bh>
2024-04-29 00:03:42 +0300

    fixed API route

--- stacks/EmailAPIStack.ts
+++ stacks/EmailAPIStack.ts
@@ -4,7 +4,7 @@ export function EmailAPIStack({ stack }: StackContext) {
     // Create the API
     const api = new Api(stack, "EmailsApi", {
         routes: {
-            "POST /send-email": "packages/functions/src/send-email.sendEmail",
+            "POST /sendemail": "packages/functions/src/email.main",
         },
     });
 
