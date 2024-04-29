06c08dfa7ad76df5bf820fe1a92b35df30210451
FatimaJ20 <fatima.janahi222@gmail.com>
2024-04-29 14:06:15 +0300

    upload somehow works (but there are errors)- i just want to save the code just incase

--- packages/frontend/src/pages/UploadEvidence.tsx
+++ packages/frontend/src/pages/UploadEvidence.tsx
@@ -5,36 +5,27 @@ import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import { FileUpload } from 'primereact/fileupload';
 
-interface ProgressLineProps {
-  activeStep: number;
-  totalSteps: number;
-}
-
-interface StepStyleProps {
-  completed: boolean;
-}
-
 const MainContainer = styled.div`
   width: 100%;
   max-width: 900px;
   margin: 0 auto;
   padding: 0 16px;
 `;
-
+ 
 const StepContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 50px;
   position: relative;
 `;
-
+ 
 const StepWrapper = styled.div`
   flex: ;
   position: relative;
   display: flex;
   justify-content: ; // Center the circle within each segment
 `;
-
+ 
 const ProgressLine = styled.div`
   height: 9px;
   background: #2ECC71;
@@ -44,8 +35,8 @@ const ProgressLine = styled.div`
   left: -4 px; // Half the size of the StepStyle to start the line right after the first circle
   z-index: -8;
 `;
-
-
+ 
+ 
 const StepStyle = styled.div`
   width: 40px;
   height: 40px;
@@ -58,29 +49,29 @@ const StepStyle = styled.div`
   position: relative; // Ensures correct positioning within StepWrapper
   z-index: 1; // Makes sure it appears above the progress line
 `;
-
+ 
 const StepCount = styled.span`
   font-size: 19px;
   color: #2ECC71;
 `;
-
+ 
 const StepsLabelContainer = styled.div`
   position: absolute;
   top: 66px;
   left: 50%;
   transform: translateX(-50%);
 `;
-
+ 
 const StepLabel = styled.span`
   font-size: 19px;
   color: #2ECC71;
 `;
-
+ 
 const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
-
+ 
 const ButtonStyle = styled.button`
   padding: 8px;
   width: 90px;
@@ -97,10 +88,8 @@ const ButtonStyle = styled.button`
 
 const UploadEvidence = () => {
   const [standards, setStandards] = useState([]);
-  //const [uploadFiles, setUploadFiles] = useState({});
   const [activeStep, setActiveStep] = useState(0);
-  const [uploadDrafts, setUploadDrafts] = useState({});
-  
+  const [isUploading, setIsUploading] = useState(false);
 
   useEffect(() => {
     const fetchStandards = async () => {
@@ -126,65 +115,47 @@ const UploadEvidence = () => {
         toast.error(`Error fetching standards: ${error.message}`);
       }
     };
-  
+
     fetchStandards();
   }, []);
-  
 
-  useEffect(() => {
-    console.log("Component mounted, loading drafts from local storage");
-    const loadedDrafts = loadFromLocalStorage('uploadDrafts');
-    if (loadedDrafts) {
-      console.log("Drafts loaded successfully:", loadedDrafts);
-      setUploadDrafts(loadedDrafts);
-    } else {
-      console.log("No drafts found in local storage.");
+  const handleFileChange = async (files:any, standard:any) => {
+    setIsUploading(true);
+    for (let file of files) {
+      if (file.size > 10000000) { // 10 MB limit
+        toast.error('File size should not exceed 10 MB');
+        continue;
+      }
+
+      const formData = new FormData();
+      formData.append('file', file);
+      const folderName = standard.standardName.replace(/[^a-zA-Z0-9]/g, '');
+
+      try {
+        const response = await fetch(' https://l1ca6m1ik7.execute-api.us-east-1.amazonaws.com/uploadS3', {
+          method: 'POST',
+          body: formData,
+          headers: new Headers({
+            'file-name': encodeURIComponent(file.name),
+            'bucket-name': 'uni-artifacts',
+            'folder-name': 'bahrainPolyTechnic',
+            'subfolder-name': encodeURIComponent(`${standard.standardId}_${folderName}`),
+          }),
+        });
+      
+        if (!response.ok) {
+          const errorText = await response.text();
+          throw new Error(`Failed to upload file: ${errorText}`);
+        }
+      
+        toast.success('File uploaded successfully');
+      } catch (error:any) {
+        console.error('Upload error:', error);
+        toast.error('Upload error: ' + error.message);
+      }
+      
     }
-  }, []);
-
-  const handleFileSelect = (e, indicatorIndex) => {
-    const selectedFiles = e.files ? e.files.map(file => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file); // Converts the file to a data URL
-        return new Promise(resolve => {
-            reader.onload = () => resolve({
-                name: file.name,
-                size: file.size,
-                type: file.type,
-                dataUrl: reader.result // This is the data URL
-            });
-        });
-    }) : [];
-
-    Promise.all(selectedFiles).then(filesWithPreview => {
-        setUploadFiles(prevUploads => {
-            const updatedUploads = { ...prevUploads };
-            if (!updatedUploads[activeStep]) updatedUploads[activeStep] = {};
-            updatedUploads[activeStep][indicatorIndex] = filesWithPreview;
-            return updatedUploads;
-        });
-
-        saveDrafts(activeStep, indicatorIndex, filesWithPreview);
-    });
-};
-
-
-  const saveDrafts = (step, index, files) => {
-    const newDrafts = { ...uploadDrafts };
-    if (!newDrafts[step]) newDrafts[step] = {};
-    newDrafts[step][index] = files;
-    console.log('Saving drafts:', newDrafts);
-    setUploadDrafts(newDrafts);
-    saveToLocalStorage('uploadDrafts', newDrafts);
-  };
-
-  const saveToLocalStorage = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
-
-  const loadFromLocalStorage = key => {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
+    setIsUploading(false);
   };
 
   const nextStep = () => {
@@ -194,34 +165,6 @@ const UploadEvidence = () => {
   const prevStep = () => {
     setActiveStep(prevStep => prevStep - 1);
   };
-
-  const deleteFile = (step, index, fileIndex) => {
-    setUploadFiles(currentFiles => {
-      const updatedFiles = { ...currentFiles };
-      if (updatedFiles[step] && updatedFiles[step][index]) {
-        // Remove the file from the list
-        updatedFiles[step][index].splice(fileIndex, 1);
-        // Optionally, check if the list is empty and handle accordingly
-        if (updatedFiles[step][index].length === 0) {
-          delete updatedFiles[step][index];
-        }
-      }
-      return updatedFiles;
-    });
-  
-    setUploadDrafts(currentDrafts => {
-      const updatedDrafts = { ...currentDrafts };
-      if (updatedDrafts[step] && updatedDrafts[step][index]) {
-        updatedDrafts[step][index].splice(fileIndex, 1);
-        if (updatedDrafts[step][index].length === 0) {
-          delete updatedDrafts[step][index];
-        }
-      }
-      saveToLocalStorage('uploadDrafts', updatedDrafts);
-      return updatedDrafts;
-    });
-  };
-  
 
   return (
     <DefaultLayout>
@@ -242,34 +185,30 @@ const UploadEvidence = () => {
         </StepContainer>
         <ButtonsContainer>
           <ButtonStyle onClick={prevStep} disabled={activeStep === 0}>Previous</ButtonStyle>
-          <ButtonStyle onClick={nextStep} disabled={activeStep === standards.length - 1}>Next</ButtonStyle>
+          <ButtonStyle onClick={nextStep} disabled={isUploading || activeStep === standards.length - 1}>
+            {isUploading ? 'Uploading...' : 'Next'}
+          </ButtonStyle>
         </ButtonsContainer>
         <br /><br /><br /><br /><br />
         {standards[activeStep]?.indicators.map((indicator, index) => (
-  <div key={`${activeStep}-${index}`} className="card">
-    <h4>{indicator.label}</h4>
-    <FileUpload 
-      name={`upload-${activeStep}-${index}`}
-      multiple 
-      accept="*"
-      maxFileSize={10000000}
-      customUpload={true}
-      onSelect={(e) => handleFileSelect(e, index)}
-      emptyTemplate={<p>Drag and drop files here to upload</p>}
-    />
-    {uploadDrafts[activeStep] && uploadDrafts[activeStep][index] && (
-      <ul>
-        {uploadDrafts[activeStep][index].map((file, fileIdx) => (
-          <li key={fileIdx}>
-            {file.name} - {file.size} bytes
-            <button onClick={() => deleteFile(activeStep, index, fileIdx)}>Delete</button>
-          </li>
+          <div key={`${activeStep}-${index}`} className="card">
+            <h4>{indicator.label}</h4>
+            <FileUpload
+              name={`upload-${activeStep}-${index}`}
+              multiple
+              accept="*"
+              maxFileSize={10000000}  // 10 MB
+              auto={false}  // Keep buttons but manage upload start manually
+              customUpload={true}
+              onSelect={(e) => handleFileChange(e.files, standards[activeStep])}
+              onError={(e) => {
+                console.error('Upload Error:', e);
+                toast.error('Upload failed: ' + e.xhr.response);
+              }}
+              emptyTemplate={<p>Drag and drop files here to upload</p>}
+            />
+          </div>
         ))}
-      </ul>
-    )}
-  </div>
-))}
-
       </MainContainer>
     </DefaultLayout>
   );
