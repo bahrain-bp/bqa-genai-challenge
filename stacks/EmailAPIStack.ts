8663382c63279b6470339ba33c595cbb1496da42
Maryam Kameshki <74730385+Mrymk02@users.noreply.github.com>
2024-05-06 14:50:15 +0300

    added ses permissions to allow lambda to access ses

--- stacks/EmailAPIStack.ts
+++ stacks/EmailAPIStack.ts
@@ -1,11 +1,29 @@
 import { Api, StackContext } from "sst/constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export function EmailAPIStack({ stack }: StackContext) {
+
+    // // Define IAM policy for SES
+    // const sesPolicy = new iam.Policy(stack, 'SESPermission', {
+    //     statements: [
+    //         new iam.PolicyStatement({
+    //             actions: ['ses:SendEmail', 'ses:SendRawEmail'],
+    //             effect: iam.Effect.ALLOW,
+    //             resources: ['*'],
+    //         }),
+    //     ],
+    // });
+
     // Create the API
     const api = new Api(stack, "EmailsApi", {
         routes: {
-            "POST /send-email": "packages/functions/src/send-email.sendEmail",
-        },
+            "POST /send-email": {
+                function: {
+                  handler: "packages/functions/src/send-email.sendEmail",
+                  permissions: ["ses"]
+                }
+              },
+        },        
     });
 
     // Show the API endpoint in the output
