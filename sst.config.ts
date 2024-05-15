447d8b6e7883484eb9a4685cb2a855866b88e3fa
Maryam Kameshki <74730385+Mrymk02@users.noreply.github.com>
2024-05-15 16:16:40 +0300

    fixed sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -7,8 +7,6 @@ import { DBStack } from "./stacks/DBStack";
 
 import { S3Stack } from "./stacks/S3Stack"
 
-import { StandardAPIStack } from "./stacks/StandardAPIStack";
-import { EmailAPIStack } from "./stacks/EmailAPIStack";
 //import { S3Stack } from "./stacks/S3Stack"
 import { ImageBuilderForCodeCatalyst } from "./stacks/devops/ImageBuilderForCodeCatalyst";
 import { OIDCForGitHubCI } from "./stacks/devops/OIDCForGitHubCI";
