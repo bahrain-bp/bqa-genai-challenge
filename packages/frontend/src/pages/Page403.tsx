c1a7856a16499cd11e0d4fdfc89185200e8ce9e9
FatimaJ20 <145936933+FatimaJ20@users.noreply.github.com>
2024-06-03 11:42:26 +0300

    Revert "Revert "403 page""

---
+++ packages/frontend/src/pages/Page403.tsx
@@ -0,0 +1,72 @@
+import { useEffect } from 'react';
+import './styles.scss'; // Make sure to include your CSS file
+import { useNavigate } from 'react-router-dom';
+
+
+const Page403 = () => {
+  const navigate = useNavigate(); // Declare navigate here to use it anywhere in your component
+
+  const goToLogin = () => {
+    navigate('/Auth/SignInPage');
+  };
+
+  useEffect(() => {
+    const interval = 500;
+
+    function generatePosition() {
+      const x = `${Math.round((Math.random() * 100) - 10)}%`;
+      const y = `${Math.round(Math.random() * 100)}%`;
+      return [x, y];
+    }
+
+   
+  
+
+    function generateLocks() {
+      const lock = document.createElement('div'),
+            position = generatePosition();
+      lock.innerHTML = '<div class="top"></div><div class="bottom"></div>';
+      lock.style.position = 'absolute';
+      lock.style.left = position[0];
+      lock.style.top = position[1];
+      lock.className = 'lock';
+      document.body.appendChild(lock);
+
+      setTimeout(() => {
+        lock.style.opacity = '1';
+        lock.classList.add('generated');
+      }, 100);
+
+      setTimeout(() => {
+        if (lock.parentElement) {
+          lock.parentElement.removeChild(lock);
+        }
+      }, 2000);
+    }
+
+    const lockInterval = setInterval(generateLocks, interval);
+    generateLocks();
+
+    return () => {
+      clearInterval(lockInterval);
+    };
+  }, []);
+
+  return (
+    <div className="container" >
+      <h1>
+        4
+        <div className="lock">
+          <div className="top"></div>
+          <div className="bottom"></div>
+        </div>
+        3
+      </h1>
+      <p>Access denied</p>
+      <button onClick={goToLogin} className="login-button">Click here to go back to login page</button> 
+
+    </div>
+  );
+};
+
+export default Page403;
